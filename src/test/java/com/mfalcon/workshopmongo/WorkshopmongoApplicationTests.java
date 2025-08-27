c9e578ed92fad0f6cad887c3b8fb5ee898bc4dbf
Batistalb <lb.gentil@gmail.com>
2025-08-27 09:45:42 -0400

    first commit

---
+++ src/test/java/com/mfalcon/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfalcon.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
